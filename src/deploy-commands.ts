4c9ed9f79ad104549ac9488651989906db1e3a16
BborisB <b.boris.2001@gmail.com>
2025-03-01 22:12:44 +0100

    feat(deploy): add command deployment script (#15)

---
+++ src/deploy-commands.ts
@@ -0,0 +1,21 @@
+import { REST, Routes } from "discord.js";
+import { config } from "dotenv";
+import { data as feedbackInitCommand } from "./commands/feedback-init";
+
+config();
+
+const commands = [feedbackInitCommand.toJSON()];
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN as string);
+
+(async () => {
+	try {
+		console.log("Déploiement des commandes...");
+		await rest.put(
+			Routes.applicationCommands(process.env.BOT_ID!),
+			{ body: commands }
+		);
+		console.log("Commandes enregistrées !");
+	} catch (error) {
+		console.error(error);
+	}
+})();
