40650c9757d19f3c1f2a6e97b377eadac47e53cc
BborisB <b.boris.2001@gmail.com>
2025-03-02 19:25:02 +0100

    feat(commands): enhance feedback-init command (#14)

--- src/commands/feedback-init.ts
+++ src/commands/feedback-init.ts
@@ -2,7 +2,7 @@ import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors, CommandInteractio
 
 export const data = new SlashCommandBuilder()
 	.setName("feedback-init")
-	.setDescription("Envoie le message permettant la création d'un sondage.");
+	.setDescription("Envoie le message permettant la création d'un questionnaire.");
 
 export async function execute(interaction: CommandInteraction) {
 	// Répond à l'intéraction, et supprime aussitot la réponse
@@ -12,18 +12,37 @@ export async function execute(interaction: CommandInteraction) {
 
 	// Création de l'embed
 	const embed = new EmbedBuilder()
-		.setTitle("💬 Questionnaire")
-		.setDescription("Cliquez sur le bouton ci-dessous pour créer un questionnaire.")
+		.setTitle("💬  Questionnaire")
+		.setDescription(
+			  "- **Créer un questionnaire**\n"
+			+ "  Démarre la création d'un questionnaire\n\n"
+			+ "- **Créer un modèle de questionnaire**\n"
+			+ "  Démarre la création d'un modèle de questionnaire\n\n"
+			+ "- **Modifier un modèle de questionnaire**\n"
+			+ "  Démarre la modification d'un modèle de questionnaire"
+		);
 
-	// Création du bouton
-	const button = new ButtonBuilder()
-		.setCustomId("feedback_button")
-		.setLabel("Donner un feedback")
+	// Création des bouton
+	const button1 = new ButtonBuilder()
+		.setCustomId("feedbackCreateButton")
+		.setLabel("Créer un questionnaire")
+		.setStyle(ButtonStyle.Primary);
+	
+	const button2 = new ButtonBuilder()
+		.setCustomId("feedbackTemplateCreateButton")
+		.setLabel("Créer un modèle de questionnaire")
 		.setStyle(ButtonStyle.Primary);
 
-	const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button);
+	const button3 = new ButtonBuilder()
+		.setCustomId("feedbackTemplateEditButton")
+		.setLabel("Modifier un modèle de questionnaire")
+		.setStyle(ButtonStyle.Primary);
+
+	const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button1, button2, button3);
 
 	// Envoi du message avec le bouton (sans la mention de la commande, en utilisant channel.send directement)
-	const channel = interaction.channel as PartialTextBasedChannelFields;
-	await channel.send({ embeds: [embed], components: [row]});
+	const channel = interaction.channel;
+	if (channel?.isSendable()) {
+		await channel.send({ embeds: [embed], components: [row] });
+	}
 }
