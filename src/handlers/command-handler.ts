88990b18b276e8c6c2f35c62b1ec15c47c8cf35e
BborisB <b.boris.2001@gmail.com>
2025-03-02 19:25:35 +0100

    feat(handlers): update command handler to use ChatInputCommandInteraction (#14)

--- src/handlers/command-handler.ts
+++ src/handlers/command-handler.ts
@@ -1,16 +1,13 @@
-import { Interaction } from "discord.js";
-import commands from '../get-commands';
+import { ChatInputCommandInteraction } from "discord.js";
+import commands from '../commands/get-commands';
 
-export async function handleCommand(interaction: Interaction) {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = commands.get(interaction.commandName);
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: "Une erreur est survenue.", ephemeral: true });
-  }
+export async function handleCommand(interaction: ChatInputCommandInteraction) {
+	const command = commands.get(interaction.commandName);
+	if (!command) return;
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: "Une erreur est survenue.", ephemeral: true });
+	}
 }
