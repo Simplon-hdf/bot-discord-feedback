8c36adc4302cdce265d4102d3dcc37a9bf0eb6ad
BborisB <b.boris.2001@gmail.com>
2025-03-02 19:45:40 +0100

    feat(events): add button and modal handlers (#22)

--- src/events/interaction.ts
+++ src/events/interaction.ts
@@ -1,14 +1,16 @@
-import { Client, Events, Interaction, ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from "discord.js";
+import { Client, Events, Interaction } from "discord.js";
 import { handleCommand } from "../handlers/command-handler";
+import { handleButton } from "../handlers/button-handler";
+import { handleModal } from "../handlers/modal-handler";
 
 export function registerInteractionEvent(client: Client) {
 	client.on(Events.InteractionCreate, async (interaction: Interaction) => {
 		if (interaction.isChatInputCommand()) {
 			await handleCommand(interaction);
 		} else if (interaction.isButton()) {
-			if (interaction.customId === "feedback_button") {
-				
-			}
+			await handleButton(interaction);
+		} else if (interaction.isModalSubmit()) {
+			await handleModal(interaction);
 		}
 	});
 }
