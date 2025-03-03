22e71f1df6a161d8db6e1c8ce762382fa72c6095
Srekaens <justindidelot8@outlook.fr>
2025-03-03 15:22:42 +0100

    feat(handlers): update button handler to support question addition (#29)

--- src/handlers/button-handler.ts
+++ src/handlers/button-handler.ts
@@ -1,8 +1,11 @@
 import { ButtonInteraction } from "discord.js";
 import { createPoll } from "./buttons/create-poll";
+import { addQuestion } from "./buttons/add-question";
 
 export async function handleButton(interaction: ButtonInteraction) {
 	if (interaction.customId === "feedbackCreateButton") {
 		await createPoll(interaction);
+	} else if (interaction.customId === "feedbackQuestionAddButton") {
+		await addQuestion(interaction);
 	}
 }
