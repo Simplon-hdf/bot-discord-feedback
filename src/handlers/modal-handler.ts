0435361e14e1eca70bd37d13a9c24e1ead6076ae
Srekaens <justindidelot8@outlook.fr>
2025-03-03 15:23:43 +0100

    feat(handlers): update modal handler to support question addition (#29)

--- src/handlers/modal-handler.ts
+++ src/handlers/modal-handler.ts
@@ -1,8 +1,11 @@
 import { ModalSubmitInteraction } from "discord.js";
 import { createPoll } from "./modals/create-poll";
+import { addQuestion } from "./modals/add-question";
 
 export async function handleModal(interaction: ModalSubmitInteraction) {
     if (interaction.customId === "pollTitleModal") {
-        createPoll(interaction);
+        await createPoll(interaction);
+    } else if (interaction.customId === "addQuestionModal") {
+        await addQuestion(interaction);
     }
 }
