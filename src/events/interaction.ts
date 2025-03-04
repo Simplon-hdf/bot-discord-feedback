7f8b20e04f44e734e06a33e61d96c9c8bcb134e8
Srekaens <justindidelot8@outlook.fr>
2025-03-04 17:21:14 +0100

    feat(events): update interaction handler to support select menus (#30)

--- src/events/interaction.ts
+++ src/events/interaction.ts
@@ -2,6 +2,7 @@ import { Client, Events, Interaction } from "discord.js";
 import { handleCommand } from "../handlers/command-handler";
 import { handleButton } from "../handlers/button-handler";
 import { handleModal } from "../handlers/modal-handler";
+import { handleSelectMenu } from "../handlers/select-menu-handler";
 
 export function registerInteractionEvent(client: Client) {
 	client.on(Events.InteractionCreate, async (interaction: Interaction) => {
@@ -11,6 +12,8 @@ export function registerInteractionEvent(client: Client) {
 			await handleButton(interaction);
 		} else if (interaction.isModalSubmit()) {
 			await handleModal(interaction);
+		} else if (interaction.isStringSelectMenu()) {
+			await handleSelectMenu(interaction);
 		}
 	});
 }
