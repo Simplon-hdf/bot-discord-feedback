d14802301ae372607c8a16ea6cd7ef969a7146aa
Boris Betremieux <b.boris.2001@gmail.com>
2025-03-07 10:37:06 +0100

    feat(utils): add poll and question embed functions

--- src/utils/components.ts
+++ src/utils/components.ts
@@ -1,4 +1,39 @@
-import { ButtonBuilder, ButtonStyle, ActionRowBuilder } from "discord.js";
+import { ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder } from "discord.js";
+import { Poll } from "../models/poll";
+import { Question } from "../models/question";
+
+export function pollEmbed(poll: Poll): EmbedBuilder {
+	const embed = new EmbedBuilder()
+		.setTitle(`Titre : ${poll.title}`)
+		.setFooter({
+			text: "Utilisez les boutons ci-dessous pour gérer les questions du sondage"
+		});
+	if (poll.questions.length === 0) {
+		embed.addFields({ name: "⚠️ Aucune question ajoutée", value: "Ajoutez-en avec le bouton ci-dessous !" });
+	} else {
+		poll.questions.forEach((question, index) => {
+			embed.addFields({ name: `Question ${index + 1}`, value: question.content, inline: false });
+		});
+	}
+	return embed;
+}
+
+export function questionEmbed(question: Question): EmbedBuilder {
+	const embed = new EmbedBuilder()
+		.setTitle(`Question : ${question.content}`)
+		.setFooter({
+			text: "Utilisez les boutons ci-dessous pour gérer les propositions de la question"
+		});
+	if (question.answers.length === 0) {
+		embed.addFields({ name: "⚠️ Aucune proposition ajoutée", value: "Ajoutez-en avec le bouton ci-dessous !" });
+	} else {
+		question.answers.forEach((answer, index) => {
+			embed.addFields({ name: `Proposition ${index + 1}`, value: answer.content, inline: false });
+		});
+	}
+	return embed;
+
+}
 
 export function initRow(): ActionRowBuilder<ButtonBuilder> {
 	const createPollButton = new ButtonBuilder()
@@ -19,6 +54,11 @@ export function initRow(): ActionRowBuilder<ButtonBuilder> {
 }
 
 export function pollRow1(): ActionRowBuilder<ButtonBuilder> {
+	const editTitleButton = new ButtonBuilder()
+		.setCustomId("feedbackEditTitleButton")
+		.setLabel("Modifier le titre du questionnaire")
+		.setStyle(ButtonStyle.Primary);
+
 	const addQuestionButton = new ButtonBuilder()
 		.setCustomId("feedbackQuestionAddButton")
 		.setLabel("Ajouter une question")
@@ -33,5 +73,6 @@ export function pollRow1(): ActionRowBuilder<ButtonBuilder> {
 		.setCustomId("feedbackQuestionRemoveButton")
 		.setLabel("Supprimer une ou plusieurs questions")
 		.setStyle(ButtonStyle.Primary);
-	return new ActionRowBuilder<ButtonBuilder>().addComponents(addQuestionButton, editQuestionButton, removeQuestionButton);
+
+	return new ActionRowBuilder<ButtonBuilder>().addComponents(editTitleButton, addQuestionButton, editQuestionButton, removeQuestionButton);
 }
