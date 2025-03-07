9dd9313df5652f216a47fb576d4ab10f000d1ba9
Boris Betremieux <b.boris.2001@gmail.com>
2025-03-07 10:35:44 +0100

    feat(handlers): integrate poll object and embed in creation

--- src/handlers/modals/create-poll.ts
+++ src/handlers/modals/create-poll.ts
@@ -1,19 +1,14 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageFlags, ModalSubmitInteraction } from "discord.js";
-import { pollRow1 } from "../../utils/components";
+import { MessageFlags, ModalSubmitInteraction } from "discord.js";
+import { pollEmbed, pollRow1 } from "../../utils/components";
+import { createPollObject } from "../../utils/poll-store";
 
 export async function createPoll(interaction: ModalSubmitInteraction) {
-	const embed = new EmbedBuilder()
-		.setTitle(`Titre : ${interaction.fields.getTextInputValue('pollTitle')}`)
-		.setDescription(
-			"Aucune question n'a encore été ajoutée à ce sondage."
-		)
-		.setFooter({
-			text: "Utilisez les boutons ci-dessous pour gérer les questions du sondage"
-		});
+
+	const poll = createPollObject(interaction.user.id, interaction.fields.getTextInputValue('pollTitle'));
 
 	// Envoyer le sondage comme un message éphémère dans le canal
 	await interaction.reply({
-		embeds: [embed],
+		embeds: [pollEmbed(poll)],
 		components: [pollRow1()],
 		flags: MessageFlags.Ephemeral
 	});
