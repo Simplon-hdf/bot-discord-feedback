2541254aeaccce7fecc17f3e8cff9970c2b929f0
Boris Betremieux <b.boris.2001@gmail.com>
2025-03-07 10:37:12 +0100

    feat(utils): implement poll object management functions

---
+++ src/utils/poll-store.ts
@@ -0,0 +1,43 @@
+import { Poll } from "../models/poll";
+
+const pollMap = new Map<string, Poll>();
+
+export function createPollObject(userId: string, title: string): Poll {
+    const poll: Poll = {
+        title: title,
+        uuidMessage: "",
+        uuidAuthor: userId,
+        isAnonymous: false,
+        isClosed: false,
+        duration: 0,
+        questions: [],
+        selectedQuestions: []
+    };
+
+    pollMap.set(userId, poll);
+    return poll;
+}
+
+export function getPollObject(userId: string): Poll | undefined {
+    return pollMap.get(userId);
+}
+
+export function deletePollObject(userId: string): void {
+    pollMap.delete(userId);
+}
+
+export function addPollQuestion(poll: Poll, question: string) {
+    poll.questions.push({
+        content: question,
+        isMultipleAnswer: false,
+        answers: []
+    });
+}
+
+export function removePollQuestion(poll: Poll) {
+    poll.selectedQuestions.sort((a, b) => b - a);
+    poll.selectedQuestions.forEach(index => {
+        poll.questions.splice(index, 1);
+    });
+    poll.selectedQuestions = [];
+}
