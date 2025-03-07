c82f5bc81d60d030de7f3b2b11e4a562223e09fd
Boris Betremieux <b.boris.2001@gmail.com>
2025-03-07 10:24:14 +0100

    feat(handlers): add editPollTitle modal handler

--- src/handlers/modal-handler.ts
+++ src/handlers/modal-handler.ts
@@ -1,16 +1,19 @@
 import { ModalSubmitInteraction } from "discord.js";
 import { createPoll } from "./modals/create-poll";
 import { addQuestion } from "./modals/add-question";
+import { editPollTitle } from "./modals/edit-poll-title";
 
 export async function handleModal(interaction: ModalSubmitInteraction) {
     console.log(`Modal soumis avec customId: ${interaction.customId}`);
     
-    if (interaction.customId === "pollTitleModal") {
+    if (interaction.customId === "createPollTitleModal") {
         console.log("Traitement du modal de création de sondage");
         await createPoll(interaction);
     } else if (interaction.customId === "addQuestionModal") {
         console.log("Traitement du modal d'ajout de question");
         await addQuestion(interaction);
+    } else if (interaction.customId === "editPollTitleModal"){
+        await editPollTitle(interaction);
     } else {
         console.log(`Modal non reconnu: ${interaction.customId}`);
     }
