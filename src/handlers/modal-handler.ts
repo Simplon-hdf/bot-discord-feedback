a45987540fa7d34e7b509757bb86e0afae71db1d
Srekaens <justindidelot8@outlook.fr>
2025-03-07 18:21:46 +0100

    feat(handlers): "Update modal handler to include new modals(#34)"

--- src/handlers/modal-handler.ts
+++ src/handlers/modal-handler.ts
@@ -2,6 +2,9 @@ import { ModalSubmitInteraction } from "discord.js";
 import { createPoll } from "./modals/create-poll";
 import { addQuestion } from "./modals/add-question";
 import { editPollTitle } from "./modals/edit-poll-title";
+import { createPollTemplate } from "./modals/create-poll-template";
+import { editQuestionText } from "./modals/edit-question-text";
+import { addProposal } from "./modals/add-proposal";
 
 export async function handleModal(interaction: ModalSubmitInteraction) {
     console.log(`Modal soumis avec customId: ${interaction.customId}`);
@@ -9,11 +12,20 @@ export async function handleModal(interaction: ModalSubmitInteraction) {
     if (interaction.customId === "createPollTitleModal") {
         console.log("Traitement du modal de création de sondage");
         await createPoll(interaction);
+    } else if (interaction.customId === "createPollTemplateModal") {
+        console.log("Traitement du modal de création de template de sondage");
+        await createPollTemplate(interaction);
     } else if (interaction.customId === "addQuestionModal") {
         console.log("Traitement du modal d'ajout de question");
         await addQuestion(interaction);
     } else if (interaction.customId === "editPollTitleModal"){
         await editPollTitle(interaction);
+    } else if (interaction.customId.startsWith("editQuestionTextModal_")) {
+        console.log("Traitement du modal de modification de texte de question");
+        await editQuestionText(interaction);
+    } else if (interaction.customId.startsWith("addProposalModal_")) {
+        console.log("Traitement du modal d'ajout de proposition");
+        await addProposal(interaction);
     } else {
         console.log(`Modal non reconnu: ${interaction.customId}`);
     }
